1a65107f2fd03021587188eec7d99d838c66bd5b
Elliot Segerlind <elliot@segerlind.se>
2024-01-23 13:11:58 +0100

    Removed unused imports

--- receptsidan/src/app/services/dishes.service.ts
+++ receptsidan/src/app/services/dishes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
